48e05dc5a0941fc10e47af846c9b1dd5fbfcdb68
keisuke_kuroiwa <a@a.jp>
2023-08-29 14:05:21 +0900

    シフトマスタメンテの新規登録時の処理修正。 テーブルから抜き取る文字列を変更SHIFT_ID→PREFIX

--- src/main/java/business/logic/utils/CommonUtils.java
+++ src/main/java/business/logic/utils/CommonUtils.java
@@ -156,7 +156,8 @@ public class CommonUtils {
             newId.append(M_shain.PREFIX.getName());
         } else if (M_shift.TABLE_NAME.getName().equals(tableName)) {
             // シフトマスタ
-            newId.append(M_shift.SHIFT_ID.getName());
+        	//8/29 黒岩 newId.append内のSHIFT_IDをPREFIXに変更
+            newId.append(M_shift.PREFIX.getName());
         }
 
         newId.append(id);
